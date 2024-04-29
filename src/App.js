475d8b05bc96bbb8609760b368b2f4dc1790f0fd
Roshini464 <143497920+Roshini464@users.noreply.github.com>
2024-04-29 09:34:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* Display your custom text instead of the image */}
+//         <p>
+//           This website is hosted using s3
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Display your custom text instead of the image */}
-        <p>
-          This website is hosted using s3
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello, All!!</h1>
+        <h2>Roshini's ''Githubactions Pipeline is Successful''</h2>
       </header>
     </div>
   );
